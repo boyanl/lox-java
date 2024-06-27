616aac3b1564bc932cb3951a61f0f381622dd7e4
Boyan Lazov <boyan.lazov@ship.cars>
2024-06-27 20:04:35 +0300

    Add backward/forward word; ignore some non-printable characters

--- app/src/main/java/com/interpreters/lox/Terminal.java
+++ app/src/main/java/com/interpreters/lox/Terminal.java
@@ -1,23 +1,39 @@
 package com.interpreters.lox;
 
-import org.jline.keymap.BindingReader;
-import org.jline.keymap.KeyMap;
-import org.jline.terminal.TerminalBuilder;
-import org.jline.utils.InfoCmp;
+import static org.jline.keymap.KeyMap.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-
-import static org.jline.keymap.KeyMap.*;
-
+import org.jline.keymap.BindingReader;
+import org.jline.keymap.KeyMap;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.utils.InfoCmp;
 
 public class Terminal {
 
-    private enum TerminalOp { INSERT, UP, DOWN, LEFT, RIGHT, EOT, BACKSPACE, DELETE, GOTO_START, GOTO_END, KILL_LINE }
+  private enum TerminalOp {
+    INSERT,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    BACKSPACE,
+    DELETE,
+    GOTO_START,
+    GOTO_END,
+    BACKWARD_WORD,
+    FORWARD_WORD,
+    DELETE_WORD_FORWARD,
+    DELETE_WORD_BACKWARD,
+    KILL_LINE,
+    EOT,
+    IGNORE
+  }
 
     private final Consumer<String> handler;
+  StringBuilder line;
     int linePos = 0;
     List<String> history;
     org.jline.terminal.Terminal terminal;
@@ -41,6 +57,7 @@ public class Terminal {
                 if (line == null) {
                     return;
                 }
+        System.out.println(line);
                 this.handler.accept(line);
                 history.add(line);
             }
@@ -50,7 +67,7 @@ public class Terminal {
     private String readLine(BindingReader bindingReader, KeyMap<TerminalOp> keyMap) {
 
         linePos = 0;
-        StringBuilder line = new StringBuilder();
+    line = new StringBuilder();
         while (true) {
                 TerminalOp op = bindingReader.readBinding(keyMap);
 
@@ -121,7 +138,18 @@ public class Terminal {
                             back(toDelete);
                         }
                     }
-                }
+        case BACKWARD_WORD -> backwardWord();
+        case FORWARD_WORD -> forwardWord();
+        case DELETE_WORD_BACKWARD -> {
+          // Delete till start of current word (or previous if currently on whitespace)
+          // TODO
+        }
+        case DELETE_WORD_FORWARD -> {
+          // Delete till end of current word (or next if currently on whitespace)
+          // TODO
+        }
+        case IGNORE -> {}
+      }
             }
 
     }
@@ -131,6 +159,38 @@ public class Terminal {
         System.out.print("\b".repeat(n));
         linePos -= n;
     }
+
+  private void forward(int n) {
+
+    assert linePos + n <= line.length();
+    for (int i = 0; i < n; i++) {
+
+      System.out.print(linePos + i < line.length() ? line.charAt(linePos + i) : ' ');
+    }
+    linePos += n;
+  }
+
+  private void backwardWord() {
+
+    int newPos = linePos - 1;
+    while (newPos > 0 && !Character.isWhitespace(line.charAt(newPos - 1))) {
+
+      newPos--;
+    }
+    newPos = Math.max(newPos, 0);
+    back(linePos - newPos);
+  }
+
+  private void forwardWord() {
+
+    int newPos = linePos + 1;
+    while (newPos + 1 < line.length() && !Character.isWhitespace(line.charAt(newPos + 1))) {
+
+      newPos++;
+    }
+    newPos = Math.min(newPos, line.length());
+    forward(newPos - linePos);
+  }
 
     void print(String s) {
         System.out.print(s);
@@ -165,11 +225,20 @@ public class Terminal {
         keyMap.bind(TerminalOp.DELETE, esc() + "[3~");
         keyMap.bind(TerminalOp.GOTO_START, ctrl('a'));
         keyMap.bind(TerminalOp.GOTO_END, ctrl('e'));
+    keyMap.bind(TerminalOp.GOTO_START, esc() + "[H");
+    keyMap.bind(TerminalOp.GOTO_END, ctrl('e'));
+    keyMap.bind(TerminalOp.GOTO_END, esc() + "[F");
         keyMap.bind(TerminalOp.EOT, ctrl('d'));
         keyMap.bind(TerminalOp.KILL_LINE, ctrl('k'));
-        // TODO: Add proper back/forward word?
-        keyMap.bind(TerminalOp.LEFT, alt('b'));
-        keyMap.bind(TerminalOp.RIGHT, alt('f'));
+    keyMap.bind(TerminalOp.BACKWARD_WORD, alt('b'));
+    keyMap.bind(TerminalOp.FORWARD_WORD, alt('f'));
+    keyMap.bind(TerminalOp.DELETE_WORD_FORWARD, alt('d'));
+    keyMap.bind(TerminalOp.DELETE_WORD_BACKWARD, ctrl('w'));
+
+    // PageUp, PageDown, Print Screen, Context Menu button, Num Lock (== Escape)
+    var nonPrintable = List.of(esc() + "[5~", esc() + "[6~", esc() + "[1;2P", ctrl('P'), esc());
+    keyMap.bind(TerminalOp.IGNORE, nonPrintable); // ignore PageUp
+
         keyMap.setNomatch(TerminalOp.INSERT);
 
         return keyMap;
